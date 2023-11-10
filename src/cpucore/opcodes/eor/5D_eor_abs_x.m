9d9f92286feb3c10d9007d80323bafd4e8c1bce7
Michal Procházka <michal@prochazkaml.eu>
2023-11-10 21:47:44 +0100

    Populated ORA/EOR opcodes

---
+++ src/cpucore/opcodes/eor/5D_eor_abs_x.m
@@ -0,0 +1,3 @@
+%#incfun "src/cpucore/microcode/addr/abs_x.m" tmp16 = ~
+%#incfun "src/cpucore/microcode/memory/getbyte.m" tmp8 = tmp16
+%#incfun "src/cpucore/microcode/insn/eor.m" a = tmp8
---
+++ src/cpucore/opcodes/ora/05_ora_zpg.m
@@ -0,0 +1,3 @@
+%#incfun "src/cpucore/microcode/addr/zpg.m" tmp16 = ~
+%#incfun "src/cpucore/microcode/memory/getbyte.m" tmp8 = tmp16
+%#incfun "src/cpucore/microcode/insn/ora.m" a = tmp8
