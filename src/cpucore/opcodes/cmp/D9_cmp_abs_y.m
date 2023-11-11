78b8908905cf9e0f8e673cf1ef0136c81f601746
Michal Procházka <michal@prochazkaml.eu>
2023-11-11 21:57:56 +0100

    Well I'm a dumbass

--- src/cpucore/opcodes/cmp/D9_cmp_abs_y.m
+++ src/cpucore/opcodes/cmp/D9_cmp_abs_y.m
@@ -1,2 +1,3 @@
-%#incfun "src/cpucore/microcode/addr/abs_y.m" tmp8 = ~
+%#incfun "src/cpucore/microcode/addr/abs_y.m" tmp16 = ~
+%#incfun "src/cpucore/microcode/memory/getbyte.m" tmp8 = tmp16
 %#incfun "src/cpucore/microcode/insn/cmp.m" ~ = a tmp8
