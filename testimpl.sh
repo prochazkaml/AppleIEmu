9ad181e7ef141158518e8137a96b30107c010314
Michal Procházka <michal@prochazkaml.eu>
2023-11-11 20:11:24 +0100

    Whoops, I made some mistakes earlier

--- testimpl.sh
+++ testimpl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#./utils/mlbundle.sh build/binsearch.m 2>&1 >/dev/null | grep -v "Including"
-./utils/mlbundle.sh build/binsearch.m
+./utils/mlbundle.sh build/binsearch.m 2>&1 >/dev/null | grep -v "Including"
+#./utils/mlbundle.sh build/binsearch.m
