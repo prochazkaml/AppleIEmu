10f870b2190fc7eeaaf9b427a731d69010b9e52d
Michal Procházka <michal@prochazkaml.eu>
2023-12-16 22:45:06 +0100

    Corrected LSL/ROR behaviour when used with the accumulator

--- src/cpucore/microcode/insn/lsr.m
+++ src/cpucore/microcode/insn/lsr.m
@@ -5,12 +5,12 @@
 
 %#incdefs "src/cpucore/microcode/flags/definitions.txt"
 
-__ret1 = (__arg1 - 0x1) / 0x2;
-
 if(bitand(__arg1, 0x1) == 0x1)
 	SET_CARRY_FLAG;
 else
 	CLR_CARRY_FLAG;
 end
+
+__ret1 = (__arg1 - 0x1) / 0x2;
 
 %#incfun "src/cpucore/microcode/flags/updatenz.m" ~ = __ret1
--- src/cpucore/microcode/insn/ror.m
+++ src/cpucore/microcode/insn/ror.m
@@ -5,11 +5,11 @@
 
 %#incdefs "src/cpucore/microcode/flags/definitions.txt"
 
-__ret1 = (__arg1 - 0x1) / 0x2 + bitand(p, __CARRY_FLAG) * 0x80;
-
 if(bitand(__arg1, 0x1) == 0x1)
+	__ret1 = (__arg1 - 0x1) / 0x2 + bitand(p, __CARRY_FLAG) * 0x80;
 	SET_CARRY_FLAG;
 else
+	__ret1 = (__arg1 - 0x1) / 0x2 + bitand(p, __CARRY_FLAG) * 0x80;
 	CLR_CARRY_FLAG;
 end
 
