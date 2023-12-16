6f25062861cbd67dc6a8013ab2034de39ab4c8e5
Michal Procházka <michal@prochazkaml.eu>
2023-12-16 22:01:02 +0100

    Corrected JSR/RTS behaviour (yes, I'm running actual tests now)

--- src/cpucore/opcodes/jump/20_jsr_abs.m
+++ src/cpucore/opcodes/jump/20_jsr_abs.m
@@ -1,4 +1,4 @@
 %#incfun "src/cpucore/microcode/memory/getword.m" tmp16_2 = pc
-pc = pc + 2;
+pc = pc + 1;
 %#incfun "src/cpucore/microcode/memory/stack_push_word.m" ~ = pc
 pc = tmp16_2;
--- src/cpucore/opcodes/jump/60_rts.m
+++ src/cpucore/opcodes/jump/60_rts.m
@@ -1,1 +1,2 @@
 %#incfun "src/cpucore/microcode/memory/stack_pop_word.m" pc = ~
+pc = pc + 1;
