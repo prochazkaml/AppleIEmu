9f388639a8c22efa991c792422fc3a64f679895e
Michal Procházka <michal@prochazkaml.eu>
2023-12-16 23:02:27 +0100

    Now simulating correct stack overflow/underflow

--- src/cpucore/microcode/memory/stack_pop.m
+++ src/cpucore/microcode/memory/stack_pop.m
@@ -1,7 +1,12 @@
 % Pops a value from the stack.
 % __ret1 = popped value
 
-s = s + 0x1;
+if s == 0xFF
+	s = 0x0;
+else
+	s = s + 0x1;
+end
+
 tmp16 = uint16(s) + 0x100;
 
 %#incfun "src/cpucore/microcode/memory/getbyte.m" __ret1 = tmp16
