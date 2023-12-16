43bda3881370d5d68b4f8887ab9c6e44b988c751
Michal Procházka <michal@prochazkaml.eu>
2023-12-16 21:58:08 +0100

    Corrected PHP/PLP behaviour

--- src/cpucore/opcodes/stack/08_php.m
+++ src/cpucore/opcodes/stack/08_php.m
@@ -1,1 +1,2 @@
+tmp8 = bitand(p, 0xCF); % all bits except break and reserved
 %#incfun "src/cpucore/microcode/memory/stack_push.m" ~ = p
--- src/cpucore/opcodes/stack/28_plp.m
+++ src/cpucore/opcodes/stack/28_plp.m
@@ -1,1 +1,2 @@
 %#incfun "src/cpucore/microcode/memory/stack_pop.m" p = ~
+p = bitor(p, 0x30); % set break and reserved bits
